9ddbeb34c0b0e3ab25bde292026d973e2ce9f6cf
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-12-07 19:25:48 +0200

    Add Graph Construction And Distance Fuction

--- graphs/graph.go
+++ graphs/graph.go
@@ -4,9 +4,38 @@ import "routely/data"
 
 // Graph reperesents a graph type in memory
 type Graph struct {
+	AdjList [][]data.Road
+	ShortestPaths [][]float64
 }
 
+
 // NewGraph builds graph from vertices and edges
-func NewGraph(vertices []*data.Intersection, edges []*data.Road) *Graph {
-	return nil
+func NewGraph(vertices []*data.Intersection, edges []*data.Road)*Graph {
+	sz := len(vertices)
+	G := &Graph{}
+	G.AdjList = make([][]data.Road,sz)
+	G.ShortestPaths = make([][]float64, sz)
+
+	for i := range G.AdjList{
+		G.AdjList[i] = make([]data.Road, 0)
+		G.ShortestPaths[i] = make([]float64,0)
+	}
+
+	for _ , E := range edges{
+		E.Weight = E.Length / E.Speed
+		G.AdjList[E.From] = append(G.AdjList[E.From], *E)
+		E.From,E.To = E.To,E.From
+		G.AdjList[E.From] = append(G.AdjList[E.From], *E)
+	}
+
+	for i := range edges{
+		G.ShortestPaths = append(G.ShortestPaths, Dijkstra(*G,i))
+	}
+	return G
+}
+
+
+// Gets shortest path between 2 nodes
+func (g *Graph)Distance(from,to uint)float64{
+	return g.ShortestPaths[from][to]
 }
