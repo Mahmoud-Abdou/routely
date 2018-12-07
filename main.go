f1ec46d35f48e06f20ea8d62c7f52d7ff5b659da
Mahmoud Abdou <mahmoudabdou.eg@gmail.com>
2018-12-07 21:22:50 +0200

    add main

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+    "os"
+    "routely/graphs"
+    "routely/reader"
+    "routely/algorithm"
+)
 
+func check(e error) {
+  if e != nil {
+      panic(e)
+  }
+}
+
+func main() {
+  mapfile, err := os.Open("./Samples/map1.txt")
+  check(err)
+  queryfile, err := os.Open("./Samples/queries1.txt")
+  check(err)
+  intersections, roads, queries := reader.ReadData(mapfile, queryfile)
+  graph := graphs.NewGraph(intersections, roads)
+  circlefinder := algorithm.NewCircleFinder(intersections)
+  for _, query := range queries {
+    path := algorithm.BestPath(graph, query, circlefinder)
+  }
 }
